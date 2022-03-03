fb59287ff2e6bd8f3b63b79f9c9c9838526fdc85
Doomer77 <newzibkoff1984@gmail.com>
2022-03-03 19:25:04 +0300

    added setting eslint, prettier, husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,23 @@ import React, { FC, useEffect, useState } from 'react'
 import Header from '../shared/ui/header/Header'
 import { CircularProgress } from '@mui/material'
 
-const Home:FC = () => {
+const Home: FC = () => {
     const [page, setPage] = useState<boolean>(true)
     useEffect(() => {
         setTimeout(() => {
             setPage(false)
         }, 1000)
-    },[])
+    }, [])
 
-    return page ? <div className='loader-box'><CircularProgress color="inherit" /></div> : <div><Header/></div>
-
+    return page ? (
+        <div className='loader-box'>
+            <CircularProgress color='inherit' />
+        </div>
+    ) : (
+        <div>
+            <Header />
+        </div>
+    )
 }
-
-
-
 
 export default Home
